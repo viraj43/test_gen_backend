e68cdbecc00587a3c5abbe419cdc8d1c6741ac2c
viraj123ab <virajmishra1804@gmail.com>
2025-07-02 16:19:54 +0530

    LASSST

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173', // The URL of your frontend app
+  origin: 'test-gen-frontend-1yzz.vercel.app', // The URL of your frontend app
   credentials: true,  // Ensure cookies are allowed to be sent with requests
 };
 
